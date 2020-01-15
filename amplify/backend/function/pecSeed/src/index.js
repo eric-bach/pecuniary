9af3c52c5ee1d8f7ed9498dd2e7302f6e51baf08
Eric Bach <bach.eric@gmail.com>
2020-01-14 20:23:03 -0700

    update

--- amplify/backend/function/pecSeed/src/index.js
+++ amplify/backend/function/pecSeed/src/index.js
@@ -18,43 +18,42 @@ const apiKey = process.env.API_KEY;
 exports.handler = async event => {
   console.log("Starting to seed Pecuniary database");
 
-  //   const req = new AWS.HttpRequest(appsyncUrl, region);
+  const req = new AWS.HttpRequest(appsyncUrl, region);
 
-  //   const item = {
-  //     input: {
-  //       name: "Lambda Item",
-  //       description: "Item Generated from Lambda"
-  //     }
-  //   };
+  const item = {
+    input: {
+      name: "Lambda Item",
+      description: "Item Generated from Lambda"
+    }
+  };
 
-  //   req.method = "POST";
-  //   req.headers.host = endpoint;
-  //   req.headers["Content-Type"] = "application/json";
-  //   req.body = JSON.stringify({
-  //     query: graphqlQuery,
-  //     operationName: "createCurrencyType",
-  //     variables: item
-  //   });
+  req.method = "POST";
+  req.headers.host = endpoint;
+  req.headers["Content-Type"] = "application/json";
+  req.body = JSON.stringify({
+    query: graphqlQuery,
+    operationName: "createCurrencyType",
+    variables: item
+  });
 
-  //   if (apiKey) {
-  //     req.headers["x-api-key"] = apiKey;
-  //   } else {
-  //     const signer = new AWS.Signers.V4(req, "appsync", true);
-  //     signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
-  //   }
+  if (apiKey) {
+    req.headers["x-api-key"] = apiKey;
+  } else {
+    const signer = new AWS.Signers.V4(req, "appsync", true);
+    signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
+  }
 
-  //   const data = await new Promise((resolve, reject) => {
-  //     const httpRequest = https.request({ ...req, host: endpoint }, result => {
-  //       result.on("data", data => {
-  //         resolve(JSON.parse(data.toString()));
-  //       });
-  //     });
+  const data = await new Promise((resolve, reject) => {
+    const httpRequest = https.request({ ...req, host: endpoint }, result => {
+      result.on("data", data => {
+        resolve(JSON.parse(data.toString()));
+      });
+    });
 
-  //     httpRequest.write(req.body);
-  //     httpRequest.end();
-  //   });
+    httpRequest.write(req.body);
+    httpRequest.end();
+  });
 
-  let data = "Test";
   return {
     statusCode: 200,
     body: data
