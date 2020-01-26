0957c1f01e46c0e6e87aa5c4241407ed87b8c551
Eric Bach <bach.eric@gmail.com>
2020-01-26 13:50:55 -0700

    Add TransactionCreated Event

--- amplify/backend/function/eventBus/src/index.js
+++ amplify/backend/function/eventBus/src/index.js
@@ -5,6 +5,8 @@ const accountUpdatedEventTopic =
   process.env.SNS_EVENTBUS_ACCOUNTUPDATEDEVENTTOPICARN;
 const accountDeletedEventTopic =
   process.env.SNS_EVENTBUS_ACCOUNTDELETEDEVENTTOPICARN;
+const transactionCreatedEventTopic =
+  process.env.SNS_EVENTBUS_TRANSACTIONCREATEDEVENTTOPICARN;
 
 exports.handler = async (event, context) => {
   var sns = new AWS.SNS({ apiVersion: "2010-03-31", region: "us-west-2" });
@@ -38,37 +40,23 @@ exports.handler = async (event, context) => {
     console.log("Data: %j", data);
     console.log("User Id: %j", userId);
 
-    // TODO Dynamically change this value
     const type = "json";
+    var message = {
+      aggregateId: aggregateId,
+      version: version,
+      userId: userId,
+      data: data
+    };
+
     var topic;
-    var message;
     if (eventName === "AccountCreatedEvent") {
       topic = accountCreatedEventTopic;
-
-      message = {
-        aggregateId: aggregateId,
-        version: version,
-        userId: userId,
-        data: data
-      };
     } else if (eventName === "AccountUpdatedEvent") {
       topic = accountUpdatedEventTopic;
-
-      message = {
-        aggregateId: aggregateId,
-        version: version,
-        userId: userId,
-        data: data
-      };
     } else if (eventName === "AccountDeletedEvent") {
       topic = accountDeletedEventTopic;
-
-      message = {
-        aggregateId: aggregateId,
-        version: version,
-        userId: userId,
-        data: data
-      };
+    } else if (eventName === "TransactionCreatedEvent") {
+      topic = transactionCreatedEventTopic;
     }
 
     console.log("Sending event to SNS for further processing");
@@ -84,190 +72,8 @@ exports.handler = async (event, context) => {
   }
 
   console.log("Stream processing complete");
-
-  // let result = "";
-
-  //   for (const record of event.Records) {
-  // console.log("DynamoDB Record: %j", record.dynamodb);
-
-  // var aggregateId = record.dynamodb.OldImage
-  //   ? trim(record.dynamodb.OldImage.aggregateId.S)
-  //   : trim(record.dynamodb.NewImage.aggregateId.S);
-  // var userId = record.dynamodb.OldImage
-  //   ? trim(record.dynamodb.OldImage.userId.S)
-  //   : trim(record.dynamodb.NewImage.userId.S);
-  // var version = record.dynamodb.OldImage
-  //   ? record.dynamodb.OldImage.version.N
-  //   : record.dynamodb.NewImage.version.N;
-  // var eventName = record.dynamodb.OldImage
-  //   ? trim(record.dynamodb.OldImage.name.S)
-  //   : trim(record.dynamodb.NewImage.name.S);
-  // var data = record.dynamodb.OldImage
-  //   ? JSON.parse(trim(record.dynamodb.OldImage.data.S))
-  //   : JSON.parse(trim(record.dynamodb.NewImage.data.S));
-
-  // console.log("Aggregate Id: %j", aggregateId);
-  // console.log("Event: %j", eventName);
-  // console.log("Data: %j", data);
-
-  // console.debug("De-normalizing %j", eventName);
-
-  // if (eventName === "AccountCreatedEvent") {
-  //   let query = `mutation createAccountReadModel {
-  //       createAccountReadModel(input: {
-  //         aggregateId: "${aggregateId}"
-  //         version: ${version}
-  //         userId: "${userId}"
-  //         name: "${data.name}"
-  //         description: "${data.description}"
-  //         accountReadModelAccountTypeId: ${data.accountAccountTypeId}
-  //         createdDate: "${data.createdDate}"
-  //       })
-  //       {
-  //         aggregateId
-  //       }
-  //     }`;
-
-  //   console.debug("Mutation/Query: %j", query);
-  //   result = await graphqlOperation(query, "createAccountReadModel");
-  // } else if (eventName === "AccountUpdatedEvent") {
-  //   let query = `mutation updateAccountReadModel {
-  //       updateAccountReadModel(input: {
-  //         id: "${data.id}"
-  //         aggregateId: "${aggregateId}"
-  //         version: ${version}
-  //         userId: "${userId}"
-  //         name: "${data.name}"
-  //         description: "${data.description}"
-  //         accountReadModelAccountTypeId: ${data.accountAccountTypeId}
-  //       })
-  //       {
-  //         aggregateId
-  //       }
-  //     }`;
-
-  //   console.debug("Mutation/Query: %j", query);
-  //   result = await graphqlOperation(query, "updateAccountReadModel");
-  // } else if (eventName === "AccountDeletedEvent") {
-  //   // 1. Get all transactions matching UserId
-  //   let transactionsQuery = `query listTransactionReadModels {
-  //     listTransactionReadModels(filter: {
-  //       userId:{
-  //         eq:"${userId}"
-  //       }
-  //     }) {
-  //       items {
-  //         id
-  //         account {
-  //           id
-  //         }
-  //       }
-  //     }
-  //   }`;
-  //   console.debug("Get transactions: %j", transactionsQuery);
-  //   var transactions = await graphqlOperation(
-  //     transactionsQuery,
-  //     "listTransactionReadModels"
-  //   );
-  //   console.debug("Get transactions result: %j", transactions);
-
-  //   // 2. Delete each transaction matching Account Id
-  //   var trans = transactions.data.listTransactionReadModels.items.filter(
-  //     e => e.account.id == data.id
-  //   );
-  //   trans.forEach(async t => {
-  //     console.log(t.id);
-
-  //     let deleteTransQuery = `mutation deleteTransactionReadModel {
-  //       deleteTransactionReadModel(input: {
-  //           id: "${t.id}"
-  //         })
-  //         {
-  //           id
-  //         }
-  //       }`;
-  //     console.debug("Delete transaction: %j", deleteTransQuery);
-  //     var deleteTransResult = await graphqlOperation(
-  //       deleteTransQuery,
-  //       "deleteTransactionReadModel"
-  //     );
-  //     console.debug("Delete transaction result: %j", deleteTransResult);
-  //   });
-
-  //   // 3. Delete Account
-  //   let query = `mutation deleteAccountReadModel {
-  //       deleteAccountReadModel(input: {
-  //         id: "${data.id}"
-  //       })
-  //       {
-  //         id
-  //       }
-  //     }`;
-
-  //   console.debug("Mutation/Query: %j", query);
-  //   result = await graphqlOperation(query, "deleteAccountReadModel");
-  //   console.debug("Delete account result: %j", result);
-  // } else if (eventName === "TransactionCreatedEvent") {
-  //   let query = `mutation createTransactionReadModel {
-  //       createTransactionReadModel(input: {
-  //         aggregateId: "${aggregateId}"
-  //         version: ${version}
-  //         userId: "${userId}"
-  //         transactionDate: "${data.transactionDate}"
-  //         symbol: "${data.symbol}"
-  //         shares: ${data.shares}
-  //         price: ${data.price}
-  //         commission: ${data.commission}
-  //         transactionReadModelAccountId: "${data.transactionReadModelAccountId}"
-  //         transactionReadModelTransactionTypeId: ${data.transactionReadModelTransactionTypeId}
-  //         createdDate: "${data.createdDate}"
-  //       })
-  //       {
-  //         aggregateId
-  //       }
-  //     }`;
-
-  //   console.debug("Mutation/Query: %j", query);
-  //   result = await graphqlOperation(query, "createTransactionReadModel");
-  //}
-  // }
-
-  // console.log("Result: %j", result);
-  // console.log(`Successfully processed ${event.Records.length} records.`);
 };
 
 function trim(str) {
   return str.replace(/^"(.*)"$/, "$1");
 }
-
-// async function graphqlOperation(query, operationName) {
-//   const req = new AWS.HttpRequest(appsyncUrl, region);
-
-//   req.method = "POST";
-//   req.headers.host = endpoint;
-//   req.headers["Content-Type"] = "application/json";
-//   req.body = JSON.stringify({
-//     query: query,
-//     operationName: operationName
-//   });
-
-//   if (apiKey) {
-//     req.headers["x-api-key"] = apiKey;
-//   } else {
-//     const signer = new AWS.Signers.V4(req, "appsync", true);
-//     signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
-//   }
-
-//   const data = await new Promise((resolve, reject) => {
-//     const httpRequest = https.request({ ...req, host: endpoint }, result => {
-//       result.on("data", data => {
-//         resolve(JSON.parse(data.toString()));
-//       });
-//     });
-
-//     httpRequest.write(req.body);
-//     httpRequest.end();
-//   });
-
-//   return data;
-// }
