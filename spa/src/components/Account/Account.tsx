54fdb7c6ddf9bf299b7e9c17d24ed72ecb7e7c73
Eric Bach <bach.eric@gmail.com>
2021-10-13 18:28:02 -0600

    Call API

--- spa/src/components/Account/Account.tsx
+++ spa/src/components/Account/Account.tsx
@@ -1,7 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, Button } from 'semantic-ui-react';
 
 const Account = () => {
-  return <div>Account</div>;
+  const [accounts, setAccounts] = useState([]);
+  const [accountsLoaded, setAccountsLoaded] = useState(false);
+
+  const fetchAccounts = async () => {
+    try {
+      let response = await fetch('https://randomuser.me/api');
+      let json = await response.json();
+
+      return { success: true, data: json };
+    } catch (error) {
+      console.error(error);
+
+      return { success: false };
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      setAccountsLoaded(false);
+      let res = await fetchAccounts();
+      if (res.success) {
+        setAccounts(res.data.results[0]);
+        setAccountsLoaded(true);
+      }
+    })();
+  }, []);
+
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <h2>
+          {/* Accounts ({accounts.length})         */}
+          Accounts 1
+          <Button
+            as={Link}
+            to='/accounts/new'
+            floated='right'
+            positive
+            content='Create Account'
+            data-test='create-account-button'
+          />
+        </h2>
+      </Grid.Column>
+    </Grid>
+  );
 };
 
 export default Account;
