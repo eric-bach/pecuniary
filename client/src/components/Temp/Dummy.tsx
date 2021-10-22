2089477a68edf4d4b7376bdc5d983dc5e8c005e5
Eric Bach <bach.eric@gmail.com>
2021-10-22 10:00:20 -0600

    Subscriptions working

--- client/src/components/Temp/Dummy.tsx
+++ client/src/components/Temp/Dummy.tsx
@@ -1,85 +1,136 @@
 import React, { useState, useEffect } from 'react';
-import { gql, useSubscription } from '@apollo/client';
-import { useMutation, useQuery } from '@apollo/client';
+import { gql, useMutation, useQuery, useSubscription } from '@apollo/client';
 import { v4 as uuidv4 } from 'uuid';
 
-const initialState = { name: '', description: '' };
+import AccountReadModel from '../Account/types/Account';
 
-const Dummy = () => {
-  const LIST_EVENTS = gql`
-    query listEvents {
-      listEvents {
-        items {
-          id
-          name
-        }
+const GET_ACCOUNTS = gql`
+  query getAccountsByUser {
+    getAccountsByUser(userId: "username") {
+      id
+      aggregateId
+      version
+      name
+      description
+      bookValue
+      marketValue
+      userId
+      createdAt
+      updatedAt
+      accountType {
+        id
+        name
+        description
       }
     }
-  `;
+  }
+`;
+const CREATE_ACCOUNT = gql`
+  mutation CreateAccount($createAccountInput: CreateEventInput!) {
+    createEvent(event: $createAccountInput) {
+      id
+      aggregateId
+      name
+      version
+      data
+      userId
+      createdAt
+    }
+  }
+`;
+const createAccountInput = {
+  createAccountInput: {
+    aggregateId: uuidv4(),
+    name: 'AccountCreatedEvent',
+    data: `{"name":"test","description":"description","bookValue":0,"marketValue":0,"accountType":{"id":"1","name":"TFSA","description":"Tax Free Savings Account"}}`,
+    version: 1,
+    userId: 'username',
+    createdAt: new Date(),
+  },
+};
 
-  const { loading: listLoading, data: listData, error: listError } = useQuery(LIST_EVENTS);
+const ACCOUNT_SUBSCRIPTION = gql`
+  subscription OnCreateEvent {
+    onCreateEvent {
+      id
+      name
+      aggregateId
+      version
+      data
+      userId
+      createdAt
+    }
+  }
+`;
 
-  console.log('LIST_EVENTS: ', listData);
+const Dummy = () => {
+  const { subscribeToMore, loading: listLoading, data: listData, error: listError, refetch } = useQuery(GET_ACCOUNTS);
+  const [createAccountMutateFunction, { error: createError }] = useMutation(CREATE_ACCOUNT);
+  const { data: createSubData, error: createSubError } = useSubscription(ACCOUNT_SUBSCRIPTION);
 
-  // const CREATE_TODO = gql`
-  //   mutation createTodo($input: CreateTodoInput!) {
-  //     createTodo(input: $input) {
-  //       id
-  //       name
-  //       description
-  //     }
-  //   }
-  // `;
+  if (listError) return <div>'Error!'</div>; // You probably want to do more here!
+  if (listLoading) return <div>'loading...'</div>; // You can also show a spinner here.
 
-  //   // https://www.apollographql.com/docs/react/data/mutations/
-  //   const [addTodoMutateFunction, { error: createError }] =
-  //     useMutation(CREATE_TODO);
+  console.log('GET_ACCOUNTS: ', listData);
 
-  //   async function addTodo() {
-  //     try {
-  //       addTodoMutateFunction({ variables: { input: { todo  } } });
-  //     } catch (err) {
-  //       console.log("error creating todo:", err);
-  //     }
-  //   }
+  const handleClick = () => {
+    console.log('CREATING ACCOUNT');
 
-  //   const DELETE_TODO = gql`
-  //     mutation deleteTodo($input: DeleteTodoInput!) {
-  //       deleteTodo(input: $input) {
-  //         id
-  //         name
-  //         description
-  //       }
-  //     }
-  //   `;
+    createAccountMutateFunction({
+      variables: createAccountInput,
+    })
+      .then((res) => {
+        console.log('Account created successfully');
+      })
+      .catch((err) => {
+        console.error('Error occurred creating account');
+        console.error(err);
+      });
+  };
 
-  //   const [deleteTodoMutateFunction] = useMutation(DELETE_TODO, {
-  //     refetchQueries: [LIST_EVENTS, "listEvents"],
-  //   });
+  const handleRefresh = () => {
+    console.log('REFRESH');
 
-  //   async function removeTodo(id : any) {
-  //     try {
-  //       deleteTodoMutateFunction({ variables: { input: { id } } });
-  //     } catch (err) {
-  //       console.log("error deleting todo:", err);
-  //     }
-  //   }
+    refetch();
+  };
 
-  //   const CREATE_TODO_SUBSCRIPTION = gql`
-  //     subscription OnCreateTodo {
-  //       onCreateTodo {
-  //         id
-  //         name
-  //         description
-  //       }
-  //     }
-  //   `;
+  return (
+    <div>
+      <h1>Accounts: {listData.getAccountsByUser.length}</h1>
+      {listData.getAccountsByUser.map((d: AccountReadModel) => {
+        return <div key={d.id}>{d.id}</div>;
+      })}
+      {subscribeToMore({
+        document: ACCOUNT_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData }) => {
+          if (!subscriptionData.data) return prev;
+          const newAccountItem = subscriptionData.data.onCreateEvent;
+          console.log('newAccountItem: ', newAccountItem);
+          console.log('newAccount data: ', JSON.parse(newAccountItem.data));
+          var data = JSON.parse(newAccountItem.data);
 
-  //   const { data: createSubData, error: createSubError } = useSubscription(
-  //     CREATE_TODO_SUBSCRIPTION
-  //   );
-
-  return <div>This page will create an Account and test that the createEvent subscription is triggered</div>;
+          return Object.assign({}, prev, {
+            getAccountsByUser: {
+              id: newAccountItem.id,
+              aggregateId: newAccountItem.aggregateId,
+              version: newAccountItem.version,
+              userId: newAccountItem.userId,
+              createdAt: newAccountItem.createdAt,
+              updatedAt: newAccountItem.createdAt,
+              name: data.name,
+              description: data.description,
+              bookValue: data.bookValue,
+              marketValue: data.marketValue,
+              accountType: data.accountType,
+            },
+            ...prev,
+          });
+        },
+      })}
+      <button onClick={() => handleClick()}>Submit</button>
+      <button onClick={() => handleRefresh()}>Refresh</button>
+    </div>
+  );
 };
 
 export default Dummy;
