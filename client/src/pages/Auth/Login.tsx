b27705daab7e1ff6b7f9899fe88c5220becdbb09
Eric Bach <bach.eric@gmail.com>
2021-10-25 11:17:52 -0600

    Add authentication failed message

--- client/src/pages/Auth/Login.tsx
+++ client/src/pages/Auth/Login.tsx
@@ -4,13 +4,9 @@ import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui
 import { UserContext } from './User';
 
 const Login = () => {
+  const [message, setMessage] = useState({ visible: false, error: '' });
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [authenticationDetails] = useState({
-    authenticated: '',
-    title: '',
-    message: '',
-  });
 
   const { authenticate }: any = useContext(UserContext);
 
@@ -25,65 +21,63 @@ const Login = () => {
         window.location.pathname = '/home';
       })
       .catch((err: any) => {
-        console.error('[LOGIN] Authentication failed: ', err);
+        console.error('[LOGIN] Authentication failed');
+        setMessage({ visible: true, error: err });
       });
   };
 
   return (
-    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 480 }}>
-        <Header as='h1' color='teal' textAlign='center'>
-          <Image src='/img/logo.png' />
-          Pecuniary
-        </Header>
-        <Form size='large' onSubmit={onSubmit}>
-          <Segment>
-            <Header as='h3' color='blue'>
-              Log into your account
-            </Header>
-            <div>Manage your account</div>
-            <br />
-            <Form.Input
-              fluid
-              icon='user'
-              iconPosition='left'
-              placeholder='E-mail address'
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-            <Form.Input
-              fluid
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password'
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
-            <Button type='submit' color='blue' fluid size='large'>
-              Login
-            </Button>
-            <br />
-            By continuing I agree to Pecuniary's <a href='/service'>Terms of Service</a>.
-            <br />
-            <br />
-            <br />
-            <a href='/reset'>Forgot your password?</a>
-            <br />
-            <br />
-            <a href='/signup'>Create new account</a>
-            <br />
-            <br />
-          </Segment>
-        </Form>
-        {authenticationDetails.authenticated !== 'true' && authenticationDetails.authenticated && (
-          <Message negative>
-            <Message.Header>{authenticationDetails.title}</Message.Header>
-            <p>{authenticationDetails.message}</p>
-          </Message>
-        )}
-      </Grid.Column>
-    </Grid>
+    <>
+      <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 480 }}>
+          <Header as='h1' color='teal' textAlign='center'>
+            <Image src='/img/logo.png' />
+            Pecuniary
+          </Header>
+          <Form size='large' onSubmit={onSubmit}>
+            <Segment>
+              <Header as='h3' color='blue'>
+                Log into your account
+              </Header>
+              <div>Manage your account</div>
+              <br />
+              <Form.Input
+                fluid
+                icon='user'
+                iconPosition='left'
+                placeholder='E-mail address'
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              <Form.Input
+                fluid
+                icon='lock'
+                iconPosition='left'
+                placeholder='Password'
+                type='password'
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <Button type='submit' color='blue' fluid size='large'>
+                Login
+              </Button>
+              {message.visible && <Message negative header='Authentication failed' content={message.error} />}
+              <br />
+              By continuing I agree to Pecuniary's <a href='/service'>Terms of Service</a>.
+              <br />
+              <br />
+              <br />
+              <a href='/reset'>Forgot your password?</a>
+              <br />
+              <br />
+              <a href='/signup'>Create new account</a>
+              <br />
+              <br />
+            </Segment>
+          </Form>
+        </Grid.Column>
+      </Grid>
+    </>
   );
 };
 
