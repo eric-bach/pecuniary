03e6dd8920e49171ddb65689ebff7aaf0e23974a
Eric Bach <bach.eric@gmail.com>
2021-10-26 11:14:30 -0600

    Add formik and yup validation

--- client/src/pages/Auth/Login.tsx
+++ client/src/pages/Auth/Login.tsx
@@ -1,30 +1,42 @@
 import { useState, useContext } from 'react';
 import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import { UserContext } from './User';
 
 const Login = () => {
   const [message, setMessage] = useState({ visible: false, error: '' });
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
   const { authenticate }: any = useContext(UserContext);
+  const { handleSubmit, handleChange, values, errors, handleBlur } = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email().required('Please enter your Email'),
+      password: Yup.string()
+        .min(8, 'Password must be a minimum of 8 characters')
+        .matches(
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/,
+          'Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character'
+        )
+        .required('Please enter your password'),
+    }),
+    onSubmit: ({ email, password }) => {
+      authenticate(email, password)
+        .then((data: any) => {
+          console.log('[LOGIN] Authentication succeeded: ', data);
 
-  const onSubmit = (event: any) => {
-    event.preventDefault();
-
-    authenticate(email, password)
-      .then((data: any) => {
-        console.log('[LOGIN] Authentication succeeded: ', data);
-
-        // Re-direct to /home
-        window.location.pathname = '/home';
-      })
-      .catch((err: any) => {
-        console.error('[LOGIN] Authentication failed');
-        setMessage({ visible: true, error: err });
-      });
-  };
+          // Re-direct to /home
+          window.location.pathname = '/home';
+        })
+        .catch((err: any) => {
+          console.error('[LOGIN] Authentication failed');
+          setMessage({ visible: true, error: err });
+        });
+    },
+  });
 
   return (
     <>
@@ -34,7 +46,7 @@ const Login = () => {
             <Image src='/img/logo.png' />
             Pecuniary
           </Header>
-          <Form size='large' onSubmit={onSubmit}>
+          <Form size='large' onSubmit={handleSubmit}>
             <Segment>
               <Header as='h3' color='blue'>
                 Log into your account
@@ -42,21 +54,27 @@ const Login = () => {
               <div>Manage your account</div>
               <br />
               <Form.Input
+                id='email'
                 fluid
                 icon='user'
                 iconPosition='left'
-                placeholder='E-mail address'
-                value={email}
-                onChange={(event) => setEmail(event.target.value)}
+                placeholder={values.email || errors.email ? errors.email : 'Email address'}
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={errors.email !== undefined}
               />
               <Form.Input
+                id='password'
                 fluid
                 icon='lock'
                 iconPosition='left'
-                placeholder='Password'
+                placeholder={values.password || errors.password ? errors.password : 'Password'}
                 type='password'
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
+                value={values.password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={errors.password !== undefined}
               />
               <Button type='submit' color='blue' fluid size='large'>
                 Login
