a58e92e23e1981df89a2f1a27761a397458e46eb
Eric Bach <bach.eric@gmail.com>
2022-04-23 15:18:28 -0600

    Clean up profile menu

--- client/src/components/Nav/SignedInMenu.tsx
+++ client/src/components/Nav/SignedInMenu.tsx
@@ -18,9 +18,6 @@ const SignedInMenu = () => {
       <Image avatar spaced='right' src='https://randomuser.me/api/portraits/lego/5.jpg' />
       <Dropdown pointing='top left' text={username}>
         <Dropdown.Menu>
-          <Dropdown.Item text='Create Event' icon='plus' />
-          <Dropdown.Item text='My Events' icon='calendar' />
-          <Dropdown.Item text='My Network' icon='users' />
           <Dropdown.Item text='My Profile' icon='user' />
           <Dropdown.Item text='Settings' icon='settings' />
           <Dropdown.Item onClick={logout} text='Sign Out' icon='power' data-test='sign-out-button' />
