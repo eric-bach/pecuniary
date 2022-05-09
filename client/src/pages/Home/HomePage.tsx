c142398b0bf863f1945f3b6237bb4caefbf68070
Eric Bach <bach.eric@gmail.com>
2022-05-08 18:45:52 -0600

    Fix decimal

--- client/src/pages/Home/HomePage.tsx
+++ client/src/pages/Home/HomePage.tsx
@@ -116,19 +116,40 @@ const HomePage = () => {
             <Statistic.Group widths='three'>
               <Statistic>
                 <Statistic.Value>
-                  <NumberFormat value={c.netWorth} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={2} />
+                  <NumberFormat
+                    value={c.netWorth}
+                    displayType={'text'}
+                    thousandSeparator={true}
+                    prefix={'$'}
+                    decimalScale={2}
+                    fixedDecimalScale={true}
+                  />
                 </Statistic.Value>
                 <Statistic.Label>Net Worth</Statistic.Label>
               </Statistic>
               <Statistic color={c.profit >= 0 ? 'green' : 'red'}>
                 <Statistic.Value>
-                  <NumberFormat value={c.profit} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={2} />
+                  <NumberFormat
+                    value={c.profit}
+                    displayType={'text'}
+                    thousandSeparator={true}
+                    prefix={'$'}
+                    decimalScale={2}
+                    fixedDecimalScale={true}
+                  />
                 </Statistic.Value>
                 <Statistic.Label>Return ($)</Statistic.Label>
               </Statistic>
               <Statistic color={c.profit >= 0 ? 'green' : 'red'}>
                 <Statistic.Value>
-                  <NumberFormat value={c.profitP} displayType={'text'} thousandSeparator={true} suffix={'%'} decimalScale={1} />
+                  <NumberFormat
+                    value={c.profitP}
+                    displayType={'text'}
+                    thousandSeparator={true}
+                    suffix={'%'}
+                    decimalScale={1}
+                    fixedDecimalScale={true}
+                  />
                 </Statistic.Value>
                 <Statistic.Label>Return (%)</Statistic.Label>
               </Statistic>
