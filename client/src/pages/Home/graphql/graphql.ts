5637ec8f44fa62c6384db1472057d5af5af887f9
Eric Bach <bach.eric@gmail.com>
2022-05-22 21:01:22 -0600

    Update HomePage

--- client/src/pages/Home/graphql/graphql.ts
+++ client/src/pages/Home/graphql/graphql.ts
@@ -3,17 +3,20 @@ import { gql } from '@apollo/client';
 export const GET_ACCOUNTS = gql`
   query GetAccounts($userId: String!) {
     getAccounts(userId: $userId) {
-      userId
-      sk
-      aggregateId
-      type
-      name
-      description
-      currencies {
-        currency
-        bookValue
-        marketValue
+      items {
+        userId
+        sk
+        aggregateId
+        type
+        name
+        description
+        currencies {
+          currency
+          bookValue
+          marketValue
+        }
       }
+      lastEvaluatedKey
     }
   }
 `;
