d8e17f861ad40656e114c709752d3302717baca3
Eric Bach <43318858+eric-bach@users.noreply.github.com>
2022-09-02 18:20:07 -0600

    Add ModuleFederationPlugin for initial MFE setup (#73)
    * Intial MFE setup
    
    * Route to marketing page
    
    * Add finance app and route
    
    * Add finance
    
    * WIP Add home pages
    
    * Clean up files

---
+++ frontend/finance/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './index.css';
+
+export default () => (
+  <div className='container'>
+    <div>Name: finance</div>
+    <div>Framework: react</div>
+    <div>Language: TypeScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+);
