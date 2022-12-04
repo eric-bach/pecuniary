89a05283025050cc83c16afbe3b12bc5b4dfcf20
Eric Bach <bach.eric@gmail.com>
2022-12-04 12:32:01 -0700

    Update

--- frontend/container/cypress/support/commands.ts
+++ frontend/container/cypress/support/commands.ts
@@ -8,5 +8,5 @@ Cypress.Commands.add('login', () => {
 
   cy.get('input[name=email]').type(Cypress.env('username'));
   cy.get('input[name=password]').type(Cypress.env('password'));
-  cy.get('button[name=signin]').contains('Sign In', { timeout: 1000 }).click();
+  cy.get('button[name=signin]').contains('Sign In', { timeout: 10000 }).click();
 });
