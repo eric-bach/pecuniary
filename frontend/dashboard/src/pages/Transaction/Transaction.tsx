41ae82838662f39e775ac96757a98e418d09641a
Eric Bach <bach.eric@gmail.com>
2022-12-06 13:36:57 -0700

    Add tranasction page view

--- frontend/dashboard/src/pages/Transaction/Transaction.tsx
+++ frontend/dashboard/src/pages/Transaction/Transaction.tsx
@@ -5,12 +5,12 @@ import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 import FormHelperText from '@mui/material/FormHelperText';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { useFormik } from 'formik';
-import dayjs, { Dayjs } from 'dayjs';
 import * as yup from 'yup';
 
 const Transaction = () => {
@@ -20,20 +20,21 @@ const Transaction = () => {
     initialValues: {
       type: '',
       transactionDate: new Date(),
-      // symbol: '',
-      // shares: 0,
-      // price: 0,
+      symbol: '',
+      shares: 0,
+      price: 0,
+      commission: 0,
     },
     validationSchema: yup.object({
       type: yup.string().required('Please select a transaction type'),
       transactionDate: yup.string().required('Please enter a transaction date'),
-      // symbol: yup
-      //   .string()
-      //   .required('Please enter a symbol')
-      //   .matches(/[A-Za-z]{2,4}[\S]*/, 'Stock symbol is invalid'),
-      // shares: yup.number().required('Please enter the number of shares').min(1, 'Number of shares is invalid'),
-      // price: yup.number().required('Please enter the share price').min(0.01, 'Price is invalid'),
-      // commission: yup.string().required('Please enter the commission').min(0, 'Commission is invalid'),
+      symbol: yup
+        .string()
+        .required('Please enter a symbol')
+        .matches(/[A-Za-z]{2,4}[\S]*/, 'Stock symbol is invalid'),
+      shares: yup.number().required('Please enter the number of shares').min(1, 'Number of shares is invalid'),
+      price: yup.number().required('Please enter the share price').min(0.01, 'Price is invalid'),
+      commission: yup.string().required('Please enter the commission').min(0, 'Commission is invalid'),
     }),
     onSubmit: (values) => {
       console.log('Formik Submitting:', values);
@@ -57,7 +58,7 @@ const Transaction = () => {
             error={formik.touched.type && Boolean(formik.errors.type)}
             variant='outlined'
             margin='normal'
-            sx={{ width: '25%' }}
+            sx={{ width: '20%' }}
           >
             {transactionTypes.map((type) => (
               <MenuItem key={type} value={type}>
@@ -68,29 +69,97 @@ const Transaction = () => {
           <FormHelperText>{formik.errors.type && formik.touched.type && formik.errors.type}</FormHelperText>
         </FormControl>
 
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker
-            label='Transaction Date'
-            value={formik.values.transactionDate}
-            onChange={(value) => {
-              formik.setFieldValue('transactionDate', value);
-            }}
-            renderInput={(params) => <TextField {...params} />}
-          />
-        </LocalizationProvider>
+        <Box sx={{ width: '20%' }}>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+              label='Transaction Date'
+              value={formik.values.transactionDate}
+              onChange={(value) => {
+                formik.setFieldValue('transactionDate', value);
+              }}
+              renderInput={(params) => <TextField {...params} />}
+            />
+          </LocalizationProvider>
+        </Box>
 
-        <Button
-          id='create'
-          name='create'
-          type='submit'
-          variant='contained'
-          color='primary'
-          onClick={(e) => {
-            formik.handleSubmit;
-          }}
-        >
-          Submit
-        </Button>
+        <Box>
+          <TextField
+            id='symbol'
+            name='symbol'
+            label='Symbol'
+            placeholder='Symbol'
+            value={formik.values.symbol}
+            onChange={formik.handleChange}
+            error={formik.touched.symbol && Boolean(formik.errors.symbol)}
+            helperText={formik.touched.symbol && formik.errors.symbol}
+            variant='outlined'
+            margin='normal'
+            sx={{ width: '20%' }}
+          />
+        </Box>
+
+        <Box>
+          <TextField
+            id='shares'
+            name='shares'
+            label='Shares'
+            placeholder='Shares'
+            value={formik.values.shares}
+            onChange={formik.handleChange}
+            error={formik.touched.shares && Boolean(formik.errors.shares)}
+            helperText={formik.touched.shares && formik.errors.shares}
+            variant='outlined'
+            margin='normal'
+            sx={{ width: '20%' }}
+          />
+        </Box>
+
+        <Box>
+          <TextField
+            id='price'
+            name='price'
+            label='Price'
+            placeholder='Price'
+            value={formik.values.price}
+            onChange={formik.handleChange}
+            error={formik.touched.price && Boolean(formik.errors.price)}
+            helperText={formik.touched.price && formik.errors.price}
+            variant='outlined'
+            margin='normal'
+            sx={{ width: '20%' }}
+          />
+        </Box>
+
+        <Box>
+          <TextField
+            id='commission'
+            name='commission'
+            label='Commission'
+            placeholder='Commission'
+            value={formik.values.commission}
+            onChange={formik.handleChange}
+            error={formik.touched.commission && Boolean(formik.errors.commission)}
+            helperText={formik.touched.commission && formik.errors.commission}
+            variant='outlined'
+            margin='normal'
+            sx={{ width: '20%' }}
+          />
+        </Box>
+
+        <Box sx={{ width: '100%' }}>
+          <Button
+            id='create'
+            name='create'
+            type='submit'
+            variant='contained'
+            color='primary'
+            onClick={(e) => {
+              formik.handleSubmit;
+            }}
+          >
+            Submit
+          </Button>
+        </Box>
       </form>
     </Container>
   );
