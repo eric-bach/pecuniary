504653933c582a1f3fbd9a896c84ab362e50e35e
Eric Bach <bach.eric@gmail.com>
2022-12-06 13:25:26 -0700

    Update Datepicker

--- frontend/dashboard/src/pages/Transaction/Transaction.tsx
+++ frontend/dashboard/src/pages/Transaction/Transaction.tsx
@@ -8,41 +8,36 @@ import Button from '@mui/material/Button';
 import FormHelperText from '@mui/material/FormHelperText';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { useFormik } from 'formik';
 import dayjs, { Dayjs } from 'dayjs';
 import * as yup from 'yup';
 
 const Transaction = () => {
-  const [transactionDate, setTransactionDate] = React.useState<Dayjs | null>(dayjs(new Date()));
-
-  const handleTransactionDateChange = (date: Dayjs | null) => {
-    setTransactionDate(date);
-  };
-
   const transactionTypes: string[] = ['Buy', 'Sell'];
 
   const formik = useFormik({
     initialValues: {
       type: '',
       transactionDate: new Date(),
-      symbol: '',
-      shares: 0,
-      price: 0,
+      // symbol: '',
+      // shares: 0,
+      // price: 0,
     },
     validationSchema: yup.object({
       type: yup.string().required('Please select a transaction type'),
       transactionDate: yup.string().required('Please enter a transaction date'),
-      symbol: yup
-        .string()
-        .required('Please enter a symbol')
-        .matches(/[A-Za-z]{2,4}[\S]*/, 'Stock symbol is invalid'),
-      shares: yup.number().required('Please enter the number of shares').min(1, 'Number of shares is invalid'),
-      price: yup.number().required('Please enter the share price').min(0.01, 'Price is invalid'),
-      commission: yup.string().required('Please enter the commission').min(0, 'Commission is invalid'),
+      // symbol: yup
+      //   .string()
+      //   .required('Please enter a symbol')
+      //   .matches(/[A-Za-z]{2,4}[\S]*/, 'Stock symbol is invalid'),
+      // shares: yup.number().required('Please enter the number of shares').min(1, 'Number of shares is invalid'),
+      // price: yup.number().required('Please enter the share price').min(0.01, 'Price is invalid'),
+      // commission: yup.string().required('Please enter the commission').min(0, 'Commission is invalid'),
     }),
     onSubmit: (values) => {
       console.log('Formik Submitting:', values);
+
       //handleSubmit(values);
     },
   });
@@ -74,11 +69,12 @@ const Transaction = () => {
         </FormControl>
 
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DesktopDatePicker
-            label='Transaction date'
-            inputFormat='MM/DD/YYYY'
-            value={transactionDate}
-            onChange={handleTransactionDateChange}
+          <DatePicker
+            label='Transaction Date'
+            value={formik.values.transactionDate}
+            onChange={(value) => {
+              formik.setFieldValue('transactionDate', value);
+            }}
             renderInput={(params) => <TextField {...params} />}
           />
         </LocalizationProvider>
