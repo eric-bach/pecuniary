235d9ffd60550b3adb29c0bf2dc9f2d47029d515
Eric Bach <bach.eric@gmail.com>
2023-06-24 12:31:07 -0600

    Clean

--- client/lib/remix-stack.ts
+++ client/lib/remix-stack.ts
@@ -1,16 +1,79 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, Duration, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { AnyPrincipal, Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { BucketDeployment, CacheControl, ServerSideEncryption, Source, StorageClass } from 'aws-cdk-lib/aws-s3-deployment';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 
-export class RemixStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+import { HttpLambdaIntegration, HttpUrlIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import { HttpApi, HttpMethod } from '@aws-cdk/aws-apigatewayv2-alpha';
+
+export class RemixStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // TODO CloudFront to API GW (from Architect)
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // const remixFunction = new Function(this, 'remix-ssr', {
+    //   functionName: 'remix-ssr',
+    //   runtime: Runtime.NODEJS_18_X,
+    //   handler: 'index.handler',
+    //   code: Code.fromAsset(path.resolve(__dirname, '../server')),
+    //   memorySize: 256,
+    //   timeout: Duration.seconds(10),
+    // });
+
+    // const hostingBucket = new Bucket(this, 'remix-bucket', {
+    //   websiteIndexDocument: 'index.html',
+    //   websiteErrorDocument: 'error.html',
+    //   blockPublicAccess: {
+    //     blockPublicAcls: false,
+    //     blockPublicPolicy: false,
+    //     ignorePublicAcls: false,
+    //     restrictPublicBuckets: false,
+    //   },
+    //   removalPolicy: RemovalPolicy.DESTROY,
+    // });
+    // hostingBucket.addToResourcePolicy(
+    //   new PolicyStatement({
+    //     actions: ['s3:GetObject'],
+    //     effect: Effect.ALLOW,
+    //     resources: [hostingBucket.arnForObjects('*')],
+    //     principals: [new AnyPrincipal()],
+    //   })
+    // );
+
+    // const ssrIntegration = new HttpLambdaIntegration('ssrIntegration', remixFunction);
+    // const bucketIntegration = new HttpUrlIntegration(
+    //   'staticIntegration',
+    //   `https://${hostingBucket.bucketName}.s3.${this.region}.amazonaws.com/{proxy}`,
+    //   {
+    //     method: HttpMethod.GET,
+    //   }
+    // );
+    // const httpApi = new HttpApi(this, 'HttpApi');
+    // httpApi.addRoutes({
+    //   path: '/{proxy+}',
+    //   methods: [HttpMethod.ANY],
+    //   integration: ssrIntegration,
+    // });
+    // httpApi.addRoutes({
+    //   path: '/_static/{proxy+}',
+    //   methods: [HttpMethod.GET],
+    //   integration: bucketIntegration,
+    // });
+
+    // // S3 bucket deployment
+    // new BucketDeployment(this, 'remix-bucket-deployment', {
+    //   sources: [Source.asset('./public')],
+    //   destinationBucket: hostingBucket,
+    //   retainOnDelete: false,
+    //   contentLanguage: 'en',
+    //   storageClass: StorageClass.INTELLIGENT_TIERING,
+    //   serverSideEncryption: ServerSideEncryption.AES_256,
+    //   cacheControl: [CacheControl.setPublic(), CacheControl.maxAge(Duration.minutes(1))],
     // });
   }
 }
