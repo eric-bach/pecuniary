8c77aa597dcf078f9b1c64a5554d68ea9daa62d5
Eric Bach <bach.eric@gmail.com>
2023-07-02 18:01:19 -0600

    Test

--- frontend/app/routes/index.tsx
+++ frontend/app/routes/index.tsx
@@ -1,26 +1,14 @@
-import { useLoaderData } from '@remix-run/react';
-import { Container } from '@mui/material';
-
 export function meta() {
   return {
-    title: 'Home Page',
-    description: 'This is the Home Page!',
+    title: "Home Page",
+    description: "This is the Home Page!",
   };
 }
 
-export async function loader() {
-  return await fetch('https://goweather.herokuapp.com/weather/Edmonton');
-}
-
 export default function Index() {
-  const data = useLoaderData<typeof loader>();
-
   return (
-    <Container>
-      <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-        <h1>Welcome to Remix</h1>
-        <p>The current temperature is {data.temperature}</p>
-      </div>
-    </Container>
+    <main>
+      <h1>Welcome to Remix</h1>
+    </main>
   );
 }
