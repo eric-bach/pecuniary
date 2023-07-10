07ae70f63ee1225d5c825330e672bdb404d2760a
Eric Bach <bach.eric@gmail.com>
2023-07-09 19:10:51 -0600

    Clean up

--- infrastructure/lib/remix-stack.ts
+++ infrastructure/lib/remix-stack.ts
@@ -53,7 +53,7 @@ export class RemixStack extends Stack {
       runtime: Runtime.NODEJS_18_X,
       handler: 'handler',
       functionName: `${props.appName}-${props.envName}-RemixSSR`,
-      entry: join(__dirname, '../../frontend/server/index.js'),
+      entry: join(__dirname, '../../frontend/server/index.mjs'),
       environment: {
         NODE_ENV: 'production',
       },
