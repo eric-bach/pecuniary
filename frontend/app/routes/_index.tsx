3bbe030e378e34faab2c38aebd9c38a0263dbe69
Eric Bach <bach.eric@gmail.com>
2023-07-09 19:17:57 -0600

    Fixes

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,41 +1,21 @@
-import type { V2_MetaFunction } from "@remix-run/node";
+import type { LoaderFunction, V2_MetaFunction } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 
 export const meta: V2_MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+  return [{ title: 'New Remix App' }, { name: 'description', content: 'Welcome to Remix!' }];
+};
+
+export const loader: LoaderFunction = async () => {
+  return await fetch('https://goweather.herokuapp.com/weather/Edmonton');
 };
 
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <div>The current temperature is {data.temperature}</div>
     </div>
   );
 }
