8412f31ca8f1c264f1da4c5fd98417c3fdf8e37e
Eric Bach <bach.eric@gmail.com>
2023-08-18 15:19:02 -0600

    Test

--- frontend/sst.config.ts
+++ frontend/sst.config.ts
@@ -3,7 +3,8 @@ import { RemixSite } from 'sst/constructs';
 import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
 import { HostedZone } from 'aws-cdk-lib/aws-route53';
 
-import { StringParameter } from 'aws-cdk-lib/aws-ssm';
+const path = require('path');
+const fs = require('fs');
 
 export default {
   config(_input) {
@@ -16,8 +17,10 @@ export default {
   stacks(app) {
     app.stack(
       function Site({ stack }) {
-        const certificateArn = StringParameter.fromStringParameterName(stack, '/sst/pecuniary/prod/certificateArn', 1);
-        const hostedZoneId = StringParameter.fromStringParameterName(stack, '/sst/pecuniary/prod/hostedZoneId', 1);
+        let config;
+        if (app.stage === 'prod') {
+          config = JSON.parse(fs.readFileSync(path.join(__dirname, `${process.env.NODE_ENV}.config`)));
+        }
 
         const site = new RemixSite(stack, 'site', {
           customDomain:
@@ -25,9 +28,9 @@ export default {
               ? {
                   domainName: 'percuiary-remix-sst.ericbach.dev',
                   cdk: {
-                    certificate: Certificate.fromCertificateArn(stack, 'Certificate', certificateArn.stringValue),
+                    certificate: Certificate.fromCertificateArn(stack, 'Certificate', config.certificateArn),
                     hostedZone: HostedZone.fromHostedZoneAttributes(stack, 'MyZone', {
-                      hostedZoneId: hostedZoneId.stringValue,
+                      hostedZoneId: config.hostedZoneId,
                       zoneName: 'ericbach.dev',
                     }),
                   },
