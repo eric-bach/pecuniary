627c469188ca2ab55f138feefb20489f99192ff6
Eric Bach <bach.eric@gmail.com>
2023-08-18 14:50:13 -0600

    Test

--- frontend/constants.ts
+++
@@ -1,3 +0,0 @@
-export const CERTIFICATE_ARN = 'arn:aws:acm:us-east-1:115499684434:certificate/de6f8372-0009-4b59-ace6-1cd9a973d895';
-export const HOSTED_ZONE_ID = 'ZZ9JJHECVGLTZ';
-export const HOSTED_ZONE_NAME = 'ericbach.dev';
