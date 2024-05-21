ab5300a38a19973588c16658aac211ad65e5ea7f
Eric Bach <bach.eric@gmail.com>
2024-05-20 21:20:02 -0600

    Test

--- frontend/src/awsconfig.ts
+++ frontend/src/awsconfig.ts
@@ -1,14 +1,16 @@
-export const config = {
+import { ResourcesConfig } from 'aws-amplify';
+
+export const config: ResourcesConfig = {
   Auth: {
     Cognito: {
-      userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID,
-      userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID,
+      userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID || '',
+      userPoolClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID || '',
     },
   },
   API: {
     GraphQL: {
-      endpoint: process.env.NEXT_PUBLIC_APPSYNC_API_ENDPOINT,
-      region: process.env.NEXT_PUBLIC_APPSYNC_REGION,
+      endpoint: process.env.NEXT_PUBLIC_APPSYNC_API_ENDPOINT || '',
+      region: process.env.NEXT_PUBLIC_APPSYNC_REGION || '',
       defaultAuthMode: 'userPool' as any,
     },
   },
