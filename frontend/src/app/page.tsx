4f6c20e66eb7d5a59f5835d1ae5ba24304c75540
Eric Bach <bach.eric@gmail.com>
2024-05-20 21:22:59 -0600

    Fix build

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,22 @@
 import type { NextPage } from 'next';
-import HomePage from './(home)/page';
+import Header from '@/components/home/header';
+import Hero from '@/components/home/hero';
+import Footer from '@/components/home/footer';
+import Feature from '@/components/home/feature';
+import Testimonial from '@/components/home/testimonial';
 
 const Home: NextPage = () => {
-  return <HomePage />;
+  return (
+    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+      <Header />
+      <main className='flex-grow'>
+        <Hero />
+        <Feature />
+        <Testimonial />
+      </main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
