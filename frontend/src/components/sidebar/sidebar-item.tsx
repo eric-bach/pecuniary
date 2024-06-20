ba1e110a783331ba7bd20f6ea2fee643802e4d61
eric-bach <bach.eric@gmail.com>
2024-06-20 15:10:15 -0600

    Update spacing of styles

--- frontend/src/components/sidebar/sidebar-item.tsx
+++ frontend/src/components/sidebar/sidebar-item.tsx
@@ -22,8 +22,8 @@ export const SidebarItem = ({ icon, title, isActive, href = '' }: Props) => {
     <NextLink href={href} className='text-default-900 active:bg-none max-w-full'>
       <div
         className={clsx(
-          isActive ? 'bg-primary-100 [&_svg_path]:fill-primary-500' : 'hover:bg-default-100',
-          'flex gap-2 w-full min-h-[44px] h-full items-center px-0 rounded-xl cursor-pointer transition-all duration-150 active:scale-[0.98]'
+          isActive ? 'bg-primary-100 [&_svg_path]:fill-primary-500 px-0.5' : 'hover:bg-default-100',
+          'flex gap-2 w-full min-h-[44px] h-full items-center px-0.5 rounded-xl cursor-pointer transition-all duration-150 active:scale-[0.98]'
         )}
         onClick={handleClick}
       >
