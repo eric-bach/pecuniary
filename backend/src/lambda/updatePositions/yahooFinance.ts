1215f2714f2a37b5d0a2f7c162414f3a8db314d6
Eric Bach <bach.eric@gmail.com>
2024-12-27 20:56:04 -0700

    Fix type

--- backend/src/lambda/updatePositions/yahooFinance.ts
+++ backend/src/lambda/updatePositions/yahooFinance.ts
@@ -63,7 +63,7 @@ async function getHistorical(symbol: string, startDate: Date, endDate: Date) {
       high: d.high,
       low: d.low,
       close: d.close,
-      adjusted_close: d.adjClose,
+      adjusted_close: d.adjClose ?? d.close,
       volume: d.volume,
     });
   });
