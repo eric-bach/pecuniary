30c91693ba0d784ee3542c965c94a90c17b87d0d
Eric Bach <bach.eric@gmail.com>
2024-12-29 14:29:13 -0700

    Fix

--- backend/src/appsync/api/types.ts
+++ backend/src/appsync/api/types.ts
@@ -5,7 +5,7 @@ import {
   Aggregates,
   BankTransaction,
   Category,
-  DeleteResponse,
+  DeleteTransaction,
   GetAccountsResponse,
   GetBankTransactionsResponse,
   GetCategoriesResponse,
@@ -66,7 +66,7 @@ export type UpdateInvestmentTransactionMutation = {
 };
 
 export type DeleteTransactionMutation = {
-  deleteTransaction: DeleteResponse;
+  deleteTransaction: DeleteTransaction;
 };
 
 export type CreateCategoryMutation = {
