8c93f5b33bc689057f4e0fda8237a29a419db62d
Eric Bach <eric.bach@ama.ab.ca>
2025-02-26 15:24:59 -0700

    Update messages

--- backend/src/lambda/updateBankAccount/main.ts
+++ backend/src/lambda/updateBankAccount/main.ts
@@ -7,12 +7,12 @@ import { injectLambdaContext } from '@aws-lambda-powertools/logger/middleware';
 import middy from '@middy/core';
 import { BankTransaction } from '../../appsync/api/codegen/appsync';
 
-const logger = new Logger({ serviceName: 'updateInvestmentAccount' });
+const logger = new Logger({ serviceName: 'updateBankAccount' });
 
 const lambdaHandler = async (event: EventBridgeEvent<string, BankTransaction>): Promise<UpdateItemCommandInput> => {
   const data = parseEvent(event);
 
-  console.log(`🔔 Received event: ${JSON.stringify(data)}`);
+  logger.debug('🔔 Received event', { data: data });
 
   if (!data || !data.accountId || !data.amount) {
     throw new Error(`🛑 No data found in event ${data}`);
@@ -29,11 +29,16 @@ const lambdaHandler = async (event: EventBridgeEvent<string, BankTransaction>): 
     }),
     ReturnValues: 'UPDATED_NEW',
   };
+
+  logger.debug('Updating bank account', { data: input });
+
   const result = await dynamoDbCommand(new UpdateItemCommand(input));
 
   if (result.$metadata.httpStatusCode !== 200) {
     throw new Error(`🛑 Could not update bank account ${data.accountId}`);
   }
+
+  logger.info(`✅ Updated bank account ${data.accountId}`);
 
   return input;
 };
@@ -41,7 +46,7 @@ const lambdaHandler = async (event: EventBridgeEvent<string, BankTransaction>): 
 function parseEvent(event: EventBridgeEvent<string, BankTransaction>): BankTransaction {
   const eventString: string = JSON.stringify(event);
 
-  console.debug(`🕧 Received event: ${eventString}`);
+  logger.debug(`Parsing event: ${eventString}`);
 
   return JSON.parse(eventString).detail;
 }
