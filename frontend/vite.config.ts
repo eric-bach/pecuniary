c57efff12232092a1b3ebdf66eb032dd48e841cb
Eric Bach <bach.eric@gmail.com>
2026-02-21 20:39:23 -0700

    test

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import { cloudflare } from '@cloudflare/vite-plugin';
 import viteReact from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -10,6 +11,7 @@ export default defineConfig({
     port: 3000,
   },
   plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     tsConfigPaths(),
     tanstackStart(),
